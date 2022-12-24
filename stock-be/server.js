a0a291ec3c1686530d9a1f889f160dd00a682b18
cc79997 <gary860608@gmail.com>
2022-12-24 10:22:30 +0800

    路由靜態檔案

--- stock-be/server.js
+++ stock-be/server.js
@@ -1,11 +1,37 @@
 const express = require('express');
 // 利用 express 這個框架建立一個 web app
-const app = express()
+const app = express();
 
-app.get('/', (req, res)=>{
-    res.send('Hello World!')
+// 設定 express 處理靜態檔案
+// -> express 內建 -> 不需要安裝任何東西
+// localhost:3001/
+// app.use(express.static('./static'));
+// localhost:3001/2048/
+app.use('/2048', express.static('./2048'));
+
+// app.[Method]
+// get post put patch delete option head
+// 中間件
+app.use((req, res, next) => {
+    console.log('中間件A');
+    req.dt=new Date().toISOString()
+    next();
+});
+app.use((req, res, next) => {
+    console.log('中間件B');
+    next();
 });
 
-app.listen(3001, ()=>{
-    console.log('server is running at 3001')
+// 路由中間件
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+app.get('/a', (req, res)=>{
+    // 一些直可以從前面的中間件傳遞過來
+    console.log(req.dt)
+    res.send('a網頁的respons')
 })
+
+app.listen(3001, () => {
+    console.log('server is running at 3001');
+});
